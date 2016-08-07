31bc24ed4b108e41acdd213692ff07e82c80550f
Paul Lamere <Paul.Lamere@gmail.com>
2016-08-07 12:59:25 -0400

    improved error handling

--- web/config.js
+++ web/config.js
@@ -1,5 +1,6 @@
 "use strict";
 
 var SPOTIFY_CLIENT_ID = '1c81d0d03de148c083744f5cce782ef7';
-var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
-var SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/OrganizeYourMusic/'
+var REMOTE_SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/OrganizeYourMusic/'
+var LOCAL_SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
+var SPOTIFY_REDIRECT_URI = REMOTE_SPOTIFY_REDIRECT_URI;
