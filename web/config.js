acba0ddb961f7822ab48799af436b9350514909d
Paul Lamere <paull@spotify.com>
2019-03-05 06:10:36 -0500

    Ready to deploy

--- web/config.js
+++ web/config.js
@@ -2,5 +2,6 @@
 
 var SPOTIFY_CLIENT_ID = '1c81d0d03de148c083744f5cce782ef7';
 var REMOTE_SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/OrganizeYourMusic/'
+var REMOTE_SPOTIFY_REDIRECT_URI = 'http://organizeyourmusic.playlistmachinery.com/'
 var LOCAL_SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
 var SPOTIFY_REDIRECT_URI = REMOTE_SPOTIFY_REDIRECT_URI;
