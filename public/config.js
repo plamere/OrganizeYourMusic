2beba8613fbef24eb8846f9db344b9aa4d06fb91
CurtisCullenAWong <curtcullenwong12@gmail.com>
2026-04-03 02:52:40 +0800

    test

--- public/config.js
+++ public/config.js
@@ -3,4 +3,4 @@
 var SPOTIFY_CLIENT_ID = '1c81d0d03de148c083744f5cce782ef7';
 var SPOTIFY_REDIRECT_URI = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? window.location.origin + '/'
-    : 'https://organizeyourmusic.playlistmachinery.com/';
+    : 'https://organize-your-music.vercel.app/';
