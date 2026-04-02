b0b64861f2e0a35352ce9463f7cac73cb2b48b27
CurtisCullenAWong <curtcullenwong12@gmail.com>
2026-04-03 02:53:55 +0800

    test

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var SPOTIFY_CLIENT_ID = '1c81d0d03de148c083744f5cce782ef7';
+var SPOTIFY_CLIENT_ID = 'bfe95a91ca5b43ab8b32c86003ee0f5f';
 var SPOTIFY_REDIRECT_URI = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? window.location.origin + '/'
     : 'https://organize-your-music.vercel.app/';
